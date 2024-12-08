28e690064d67134a56cdec386f9ad86c138d2802
exosZoldyck <99753380+exosZoldyck@users.noreply.github.com>
2024-12-08 19:59:25 +0100

    Update build

--- build.bat
+++ build.bat
@@ -3,11 +3,15 @@ cls
 
 del template-gen.exe
 del terramosaic.exe
+md build
 
 echo Compiling "template-gen.cpp"...
 g++ -Wall -o template-gen template-gen.cpp lib/colors.cpp lib/mosaic.cpp -I.
 
 echo Compiling "main.cpp"...
 g++ -Wall -o terramosaic main.cpp ./lib/colors.cpp ./lib/mosaic.cpp  -I.
+
+move template-gen.exe ./build
+move terramosaic.exe ./build
 
 echo Done!
